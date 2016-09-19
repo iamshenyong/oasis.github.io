23edc9e33967837c806020bb87ca3076402c6542
sheny <iamshenyong@163.com>
2016-09-19 23:11:01 +0800

    update

---
+++ js/load_iframe.js
@@ -0,0 +1,9 @@
+﻿function load_archives() {
+	document.getElementById("content_iframe").src = "subject/archives.html";
+}
+function load_join() {
+	document.getElementById("content_iframe").src = "subject/join.html";
+}
+function load_about() {
+	document.getElementById("content_iframe").src = "subject/about.html";
+}
