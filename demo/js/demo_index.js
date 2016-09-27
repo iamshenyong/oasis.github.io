90fea1c782d5b87dbea4ef75618ef425564f4246
sheny <iamshenyong@163.com>
2016-09-27 20:58:35 +0800

    update

---
+++ demo/js/demo_index.js
@@ -0,0 +1,25 @@
+﻿function setpages(e)
+{
+	var id=e.getAttribute("frameid");
+	var src=e.getAttribute("pages");
+	document.getElementById(id).src = src;
+}
+
+function hidden_item(e)
+{
+	var id=e.getAttribute("frameid");
+	var src=e.getAttribute("pages");
+	document.getElementById(id).src = src;
+}
+
+function hidden_item(e){
+	var id=e.getAttribute("groupid");
+	var elem = document.getElementById(id).getElementsByTagName("li");
+	for(var i = 0; i < elem.length; i++){
+		if (elem[i].hasAttribute("hidden")) {
+			elem[i].removeAttribute("hidden");
+		} else {
+			elem[i].setAttribute("hidden", "hidden");
+		}
+	}
+}
